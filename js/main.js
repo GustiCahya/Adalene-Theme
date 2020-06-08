4bacc4b787114f4f86025f06bfbb9db941fbb84e
GustiCahya <gusticahyax@yahoo.com>
2020-06-08 11:27:03 +0800

    finish landingpage

--- js/main.js
+++ js/main.js
@@ -7,7 +7,21 @@ document.addEventListener('DOMContentLoaded', function(){
     M.Sidenav.init($('.sidenav'), {
         edge: 'right'
     })
+    M.Parallax.init($('.parallax'))
 })
+
+const navbarIntersecting = new IntersectionObserver(function(entries, observer){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      $('.nav-extended').style.top = '-50%';
+    }else{
+      $('.nav-extended').style.top = '0%';
+    }
+  })
+}, {
+  threshold: .4
+})
+navbarIntersecting.observe($('.best-sellers'))
 
 new Swiper('.best-sellers__items', {
   speed: 400,
@@ -24,11 +38,31 @@ new Swiper('.best-sellers__items', {
     640: {
       slidesPerView: 2
     },
-    1000: {
+    800: {
       slidesPerView: 3
     },
     1200: {
       slidesPerView: 4
+    }
+  }
+})
+
+new Swiper('.instagram-photos', {
+  speed: 400,
+  loop: true,
+  direction: 'horizontal',
+  breakpoints: {
+    320: {
+      slidesPerView: 2
+    },
+    640: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 4
+    },
+    1200: {
+      slidesPerView: 5
     }
   }
 })
